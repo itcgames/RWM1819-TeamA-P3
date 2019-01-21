f78299276b9bf1e5ce6916cbcdf5e4c08d9d80ff
sybek96 <sybek96@interia.pl>
2019-01-21 15:46:36 +0000

    started paddle object

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,10 @@
 
 class Game {
   constructor() {
+    this.paddle = new Paddle(100,100);
     this.prevDt = Date.now();
     this.canvas = new Canvas("canvas");
+    this.ctx = canvas.getContext("2d");
   }
 
   run() {
@@ -19,9 +21,12 @@ class Game {
 
   update() {
     const dt = this.calculateDt();
+    this.paddle.update(dt);
   }
 
   render() {
+    this.ctx.clearRect(0,0,this.canvas.resolution.x, this.canvas.resolution.y);
+    this.paddle.draw(this.ctx);
   }
 
   calculateDt() {
