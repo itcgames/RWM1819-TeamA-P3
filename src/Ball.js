fdef913916bc95a53b215bac9b5bc2906002b0c4
sybek96 <sybek96@interia.pl>
2019-01-24 16:05:18 +0000

    finished feature-disrtuption

--- src/Ball.js
+++ src/Ball.js
@@ -1,20 +1,43 @@
 class Ball{
-  constructor(x, y, r) {
+  constructor(x, y, r, speed, velocity) {
+    //optionally set velocity
+    if(velocity === undefined){
+      this.velocity = {
+        x:1 ,
+        y:1
+      }
+    } else {
+      this.velocity = {
+        x: velocity.x,
+        y: velocity.y
+      };
+    }
     this.position = {
       x: x,
       y: y
     }
-    this.velocity = {
-      x:1 ,
-      y:1
-    }
     this.radius = r;
-    this.speed = 0;
+    this.speed = speed === undefined ? 0 : speed;
     this.img = new Image(this.radius, this.radius);
     this.img.src = "./res/Images/Ball/ball.png";
   }
 
   update() {
+
+    var angle = Math.atan2(this.velocity.y, this.velocity.x);
+    angle = VectorMath.toDeg(angle)
+  
+    //make unit vector from angle
+    var firingVectorUnit = VectorMath.vector(angle);
+    //multiply by speed
+    var firingVector = {
+      x: firingVectorUnit.x * this.speed,
+      y: firingVectorUnit.y * this.speed
+    }
+    this.velocity.x = firingVector.x;
+    this.velocity.y = firingVector.y;
+
+
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
   }
--- src/Game.js
+++ src/Game.js
@@ -38,7 +38,7 @@ class Game {
     this.powerUpTimer1;
     this.powerUpTimer2;
     this.randomNumGen;
-    
+
     /** @type {Array<{ Bricks: Array<{ type: string, position: { x: number, y: number }, width: number, height: number }> }>} */
     this.levels = [];
 
@@ -64,7 +64,9 @@ class Game {
       this.run();
     }, ev => { alert("Failed to load level"); });
 
-    this.ball = new Ball(100, 100, 20);
+    /**@type {Array<Ball>} */
+    this.balls = [];
+    this.balls.push(new Ball(100, 100, 20, 8));
     this.ballSpawning = true;
     this.spawnBallCountdown = 3.0;
     this.generatedRandomPaddlePos = false;
@@ -80,6 +82,7 @@ class Game {
       onKeyDown: this.onKeyDown.bind(this)
     };
     window.addEventListener("keydown", this.events.onKeyDown, false);
+    this.triple = false;
   }
   /**
    * This is the function that detect key presses.
@@ -152,131 +155,123 @@ class Game {
 
         this.dnd.update();
         this.paddle.update(dt);
-        this.ballUpdate(dt);
-        if ((this.isPlayerOne ? this.players.one.score : this.players.two.score) > this.highScore) {
-          this.highScore = (this.isPlayerOne ? this.players.one.score : this.players.two.score);
-        }
-        this.bricks.forEach((brick, index, array) => {
-          brick.update();
-          if (Collision.BallToBlock(this.ball, brick)) {
-            if (this.powerUpActive === false) {
-              this.checkSpawnPowerup(brick.x + 12, brick.y);
-            }
+        this.balls.forEach((ball, index) => {
+          ball.update(dt)
+          this.ballUpdate(ball, index, dt);
+          if ((this.isPlayerOne ? this.players.one.score : this.players.two.score) > this.highScore) {
+            this.highScore = (this.isPlayerOne ? this.players.one.score : this.players.two.score);
           }
-          Collision.LasersToBlock(this.paddle.lasers, brick);
-          if (brick.health <= 0) {
-            array.splice(index, 1);
-            if (this.isPlayerOne) {
-              this.players.one.score += brick.score;
-            } else {
-              this.players.two.score += brick.score;
+          this.bricks.forEach((brick, index, array) => {
+            brick.update();
+            if (Collision.BallToBlock(ball, brick)) {
+              if (this.powerUpActive === false) {
+                this.checkSpawnPowerup(brick.x + 12, brick.y);
+              }
             }
-          }
-          this.enemies.forEach(enemy => {
-            Collision.EnemyToBlock(enemy, brick);
+            Collision.LasersToBlock(this.paddle.lasers, brick);
+            if (brick.health <= 0) {
+              array.splice(index, 1);
+              if (this.isPlayerOne) {
+                this.players.one.score += brick.score;
+              } else {
+                this.players.two.score += brick.score;
+              }
+            }
+            this.enemies.forEach(enemy => {
+              Collision.EnemyToBlock(enemy, brick);
+            });
           });
-        });
-        this.enemies.forEach(enemy => {
-          enemy.update();
+          this.enemies.forEach(enemy => {
+            enemy.update();
+            if (!this.ballSpawning) {
+              Collision.BallToEnemy(ball, enemy);
+            }
+            Collision.LasersToWorld(this.paddle.lasers, this.worldBounds.minY);
+            this.powerUp.update();
+            Collision.LasersToEnemies(this.paddle.lasers, enemy);
+            Collision.PaddleToEnemy(this.paddle, enemy);
+            if (enemy.health <= 0) {
+              array.splice(index, 1);
+              if (this.isPlayerOne) {
+                this.players.one.score += 100;
+              } else {
+                this.players.two.score += 100;
+              }
+            }
+          });
           if (!this.ballSpawning) {
-            Collision.BallToEnemy(this.ball, enemy);
+            Collision.BallToPaddle(ball, this.paddle);
           }
           Collision.LasersToWorld(this.paddle.lasers, this.worldBounds.minY);
-          this.powerUp.update();
-          if (Collision.PaddleToPowerUp(this.paddle, this.powerUp) && this.powerUp.active) {
-            if (this.powerUp.type === "SLOW") {
-              this.ball.speed -= 4;//get angle
-              var angle = Math.atan2(this.ball.velocity.y, this.ball.velocity.x);
-              angle = VectorMath.toDeg(angle)
-
-              //make unit vector from angle
-              var firingVectorUnit = VectorMath.vector(angle);
-              //multiply by speed
-              var firingVector = {
-                x: firingVectorUnit.x * this.ball.speed,
-                y: firingVectorUnit.y * this.ball.speed
-              }
-              this.ball.velocity.x = firingVector.x;
-              this.ball.velocity.y = firingVector.y;
-              this.powerUp.active = false;
-            }
+          if (this.powerUpActive === true) {
+            this.powerUp.update(dt);
+            this.powerUpTimer2 = new Date();
           }
-          Collision.LasersToEnemies(this.paddle.lasers, enemy);
-          Collision.PaddleToEnemy(this.paddle, enemy);
-          if (enemy.health <= 0) {
-            array.splice(index, 1);
-            if (this.isPlayerOne) {
-              this.players.one.score += 100;
-            } else {
-              this.players.two.score += 100;
+          if (this.powerUpTimer2 - this.powerUpTimer1 >= 10000) {
+            this.powerUpActive = false;
+          }
+          if (this.powerUpActive === true) {
+            if (Collision.PaddleToPowerUp(this.paddle, this.powerUp) && this.powerUp.active) {
+              if (this.powerUp.type === "LASER") {
+                if (this.paddle.enlargePowerActive) {
+                  this.paddle.enlargePowerActive = false;
+                }
+                this.paddle.laserPowerActive = true;
+                this.paddle.paddleAnimator.continue();
+                this.powerUp.active = false;
+              }
+              else if (this.powerUp.type === "ENLARGE") {
+                if (this.paddle.laserPowerActive) {
+                  this.paddle.laserPowerActive = false;
+                }
+                this.paddle.enlargePowerActive = true;
+                this.powerUp.active = false;
+              }
+              else if (this.powerUp.type === "CATCH") {
+                this.powerUp.active = false;
+              }
+              else if (this.powerUp.type === "SLOW") {
+                this.balls.forEach(ball =>{
+                  ball.speed -= 4;
+                  ball.img.src = "./res/Images/Ball/ball_slow.png";
+                });
+
+                this.powerUp.active = false;
+              }
+              else if (this.powerUp.type === "BREAK") {
+                this.powerUp.active = false;
+              }
+              else if (this.powerUp.type === "DISRUPTION") {
+                if(!this.triple){
+                  this.triple = true;
+                  //calculate VectorMath.vector()
+                  var angle = VectorMath.angle(ball.velocity);
+                  //offset the angle
+                  var angle1 = angle - 10;
+                  var angle2 = angle + 10;
+                  //var firingVectorUnit = VectorMath.vector(angle);
+
+                  var vel1 = VectorMath.vector(angle1);
+                  var vel2 = VectorMath.vector(angle2);
+                  this.balls.push(new Ball(ball.position.x, ball.position.y, 20, ball.speed, vel1));
+                  this.balls.push(new Ball(ball.position.x, ball.position.y, 20, ball.speed, vel2));
+                  this.powerUp.active = false;
+                }
+                
+              }
+              else if (this.powerUp.type === "PLAYER") {
+                this.powerUp.active = false;
+                if (this.isPlayerOne) {
+                  this.players.one.lives += 1;
+                } else {
+                  this.players.two.lives += 1;
+                }
+              }
             }
           }
         });
-        if (!this.ballSpawning) {
-          Collision.BallToPaddle(this.ball, this.paddle);
-        }
-        Collision.LasersToWorld(this.paddle.lasers, this.worldBounds.minY);
-        if (this.powerUpActive === true) {
-          this.powerUp.update(dt);
-          this.powerUpTimer2 = new Date();
-        }
-        if (this.powerUpTimer2 - this.powerUpTimer1 >= 10000) {
-          this.powerUpActive = false;
-        }
-        if (this.powerUpActive === true) {
-          if (Collision.PaddleToPowerUp(this.paddle, this.powerUp) && this.powerUp.active) {
-            if (this.powerUp.type === "LASER") {
-              if(this.paddle.enlargePowerActive){
-                this.paddle.enlargePowerActive = false;
-              }
-              this.paddle.laserPowerActive = true;
-              this.paddle.paddleAnimator.continue();
-              this.powerUp.active = false;
-            }
-            else if (this.powerUp.type === "ENLARGE") {
-              if(this.paddle.laserPowerActive){
-                this.paddle.laserPowerActive = false;
-              }
-              this.paddle.enlargePowerActive = true;
-              this.powerUp.active = false;
-            }
-            else if (this.powerUp.type === "CATCH") {
-              this.powerUp.active = false;
-            }
-            else if (this.powerUp.type === "SLOW") {
-              this.ball.speed -= 4;//get angle
-              var angle = Math.atan2(this.ball.velocity.y, this.ball.velocity.x);
-              angle = VectorMath.toDeg(angle)
-
-              //make unit vector from angle
-              var firingVectorUnit = VectorMath.vector(angle);
-              //multiply by speed
-              var firingVector = {
-                x: firingVectorUnit.x * this.ball.speed,
-                y: firingVectorUnit.y * this.ball.speed
-              }
-              this.ball.velocity.x = firingVector.x;
-              this.ball.velocity.y = firingVector.y;
-              this.ball.img.src = "./res/Images/Ball/ball_slow.png";
-              this.powerUp.active = false;
-            }
-            else if (this.powerUp.type === "BREAK") {
-              this.powerUp.active = false;
-            }
-            else if (this.powerUp.type === "DISRUPTION") {
-              this.powerUp.active = false;
-            }
-            else if (this.powerUp.type === "PLAYER") {
-              this.powerUp.active = false;
-              if (this.isPlayerOne) {
-                this.players.one.lives += 1;
-              } else {
-                this.players.two.lives += 1;
-              }
-            }
-          }
-        }
       }
+
     }
   }
 
@@ -288,7 +283,8 @@ class Game {
     }
     if (this.menuManager.current.key === "Game Scene") {
       this.paddle.draw(this.ctx);
-      this.ball.render(this.ctx);
+      //this.ball.render(this.ctx);
+      this.balls.forEach(ball => ball.render(this.ctx));
       this.bricks.forEach(brick => brick.draw(this.ctx));
       this.enemies.forEach(enemy => enemy.draw(this.ctx));
       if (this.powerUpActive === true) {
@@ -315,7 +311,7 @@ class Game {
    * @param {Number} dt
    * time between cycles
    */
-  ballUpdate(dt) {
+  ballUpdate(ball, index, dt) {
 
     if (this.ballSpawning) {
       this.spawnBallCountdown -= dt / 1000;
@@ -326,16 +322,16 @@ class Game {
         this.generatedRandomPaddlePos = true;
       }
       //make balls position relative to the paddle
-      this.ball.position.x = this.paddle.origin.x - (this.ball.radius / 2) + this.randPaddlePos;
-      this.ball.position.y = this.paddle.colBox.position.y - (this.ball.radius);
+      ball.position.x = this.paddle.origin.x - (ball.radius / 2) + this.randPaddlePos;
+      ball.position.y = this.paddle.colBox.position.y - (ball.radius);
       //when countdown is 0 fire ball at angle depending on position
       //relative to the paddle
       if (this.spawnBallCountdown <= 0) {
 
         //calculate vector between ball and paddle
         var vectorBetweenBallAndPaddle = {
-          x: this.ball.position.x + this.ball.radius - this.paddle.origin.x,
-          y: this.ball.position.y + this.ball.radius - this.paddle.origin.y
+          x: ball.position.x + ball.radius - this.paddle.origin.x,
+          y: ball.position.y + ball.radius - this.paddle.origin.y
         }
 
         //get angle
@@ -353,69 +349,75 @@ class Game {
         //reset variables for spawning
         this.ballSpawning = false;
         this.spawnBallCountdown = 3.0;
-        this.ball.velocity.x = firingVector.x;
-        this.ball.velocity.y = firingVector.y;
-        this.ball.speed = this.ballStartSpeed;
+        ball.velocity.x = firingVector.x;
+        ball.velocity.y = firingVector.y;
+        ball.speed = this.ballStartSpeed;
         this.generatedRandomPaddlePos = false;
       }
     }
     else {
-      this.ballWorldCollision();
-      this.ball.update();
+      this.ballWorldCollision(ball, index);
+      ball.update(dt);
     }
   }
 
-  ballWorldCollision() {
-    if (this.ball.position.x + (this.ball.radius * 2) > this.worldBounds.maxX) {
-      this.ball.flipVelX();
+  ballWorldCollision(ball, index) {
+    if (ball.position.x + (ball.radius * 2) > this.worldBounds.maxX) {
+      ball.flipVelX();
     }
-    if (this.ball.position.x < this.worldBounds.minX) {
-      this.ball.flipVelX();
+    if (ball.position.x < this.worldBounds.minX) {
+      ball.flipVelX();
     }
-    if (this.ball.position.y > this.worldBounds.maxY) {
-      this.ball.flipVelY();
+    if (ball.position.y > this.worldBounds.maxY) {
+      ball.flipVelY();
     }
-    if (this.ball.position.y < this.worldBounds.minY) {
-      this.ball.flipVelY();
+    if (ball.position.y < this.worldBounds.minY) {
+      ball.flipVelY();
     }
-    if (this.ball.position.y + (this.ball.radius * 2) > this.worldBounds.maxY) {
-      this.ballSpawning = true;
-      this.ball.img.src = "./res/Images/Ball/ball.png";
-      if (this.isPlayerOne) {
-        this.players.one.lives -= 1;
-        if (this.players.one.lives < 0)
-          this.players.one.lives = 0;
-
-      } else {
-        this.players.two.lives -= 1;
-        if (this.players.two.lives < 0)
-          this.players.two.lives = 0;
-      }
-      this.paddle.laserPowerActive = false;
-      if (this.twoPlayerMode) {
-        if ((this.isPlayerOne && this.players.two.lives > 0) || (!this.isPlayerOne && this.players.one.lives > 0)) {
-          this.isPlayerOne = !this.isPlayerOne; // swapping active player
-        } else if (this.players.one.lives > 0) {
-          // player one has lives left swap to him
-          this.isPlayerOne = true;
-        } else if (this.players.two.lives > 0) {
-          // player two has lives left swap to him
-          this.isPlayerOne = false;
+    if (ball.position.y + (ball.radius * 2) > this.worldBounds.maxY) {
+      if(this.balls.length === 1){
+        this.triple = false;
+        this.ballSpawning = true;
+        ball.img.src = "./res/Images/Ball/ball.png";
+        if (this.isPlayerOne) {
+          this.players.one.lives -= 1;
+          if (this.players.one.lives < 0)
+            this.players.one.lives = 0;
+  
         } else {
-          // Game over both players lose
+          this.players.two.lives -= 1;
+          if (this.players.two.lives < 0)
+            this.players.two.lives = 0;
+        }
+        this.paddle.laserPowerActive = false;
+        if (this.twoPlayerMode) {
+          if ((this.isPlayerOne && this.players.two.lives > 0) || (!this.isPlayerOne && this.players.one.lives > 0)) {
+            this.isPlayerOne = !this.isPlayerOne; // swapping active player
+          } else if (this.players.one.lives > 0) {
+            // player one has lives left swap to him
+            this.isPlayerOne = true;
+          } else if (this.players.two.lives > 0) {
+            // player two has lives left swap to him
+            this.isPlayerOne = false;
+          } else {
+            // Game over both players lose
+            this.menuManager.fadeTo("Main Menu");
+            this.play = false;
+          }
+          this.bricks = this.isPlayerOne
+            ? this.players.one.bricks
+            : this.players.two.bricks;
+          this.enemies = this.isPlayerOne
+            ? this.players.one.enemies
+            : this.players.two.enemies;
+        } else if (this.players.one.lives <= 0) {
+          // Game over in player one mode
           this.menuManager.fadeTo("Main Menu");
           this.play = false;
         }
-        this.bricks = this.isPlayerOne
-          ? this.players.one.bricks
-          : this.players.two.bricks;
-        this.enemies = this.isPlayerOne
-          ? this.players.one.enemies
-          : this.players.two.enemies;
-      } else if (this.players.one.lives <= 0) {
-        // Game over in player one mode
-        this.menuManager.fadeTo("Main Menu");
-        this.play = false;
+      }
+      else{
+        this.balls.splice(index, 1);
       }
     }
   }
